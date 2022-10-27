e38313da1fd72788877355dad3c2896d8e82416a
Rafael <tenorio.rafa17k@gmail.com>
2022-10-27 13:39:51 -0300

    menu responsivo

---
+++ static/script.js
@@ -0,0 +1,13 @@
+/**Criação da abertura e fechamento
+ * do menu mobile
+ */
+
+const menuMobile = document.querySelector('.menu-mobile')
+const body = document.querySelector('body')
+
+menuMobile.addEventListener('click', () =>{
+    menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list","bi-x")
+    : menuMobile.classList.replace("bi-x","bi-list")
+    body.classList.toggle("menu-nav-active")
+});
