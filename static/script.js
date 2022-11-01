1045f4acabd6bcd596bda90944f7b0fa33f9be2e
Rafael <tenorio.rafa17k@gmail.com>
2022-11-01 13:38:15 -0300

    Sobre e Habilidades

--- static/script.js
+++ static/script.js
@@ -11,3 +11,18 @@ menuMobile.addEventListener('click', () =>{
     : menuMobile.classList.replace("bi-x","bi-list")
     body.classList.toggle("menu-nav-active")
 });
+
+/** Fechamento do menu 
+ * mobile
+ */
+
+// const navItem = document.querySelector('.nav-item')
+
+// navItem.forEach(item => {
+//     item.addEventListener("click", () => {
+//         if (body.classList.contains("menu-nav-active")){
+//             body.classList.remove("menu-nav-active")
+//             menuMobile.classList.replace("bi-x","bi-list")
+//         }
+//     })
+// })
