d8aa8a9429c8a58a8b1ec7b31d8d2e64b9bbcc74
Rafael <tenorio.rafa17k@gmail.com>
2022-11-08 20:54:26 -0300

    Final

--- static/script.js
+++ static/script.js
@@ -26,3 +26,29 @@ menuMobile.addEventListener('click', () =>{
 //         }
 //     })
 // })
+
+
+//animar todos os atributos da tela que tiver data anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop){
+            element.classList.add("animate");
+        } else{
+            element.classList.remove("animate");
+        }
+    });
+
+
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
